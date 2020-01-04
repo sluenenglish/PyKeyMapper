4a595da15c77f33eeace4c6e9763ff3ae469c23e
Sam Luen-English <sluenenglish@gmail.com>
2020-01-04 14:42:01 +0000

    Mappings with other modifiers working

--- pykey/main.py
+++ pykey/main.py
@@ -4,8 +4,10 @@ from ctypes import *
 
 from .vars import CODE, VALUE, TYPE, InputEvent
 
+
 def log(text):
     print(str(text), file=sys.stderr)
+
 
 def log_event(input_event, extra=''):
     if input_event.code == 0:
@@ -28,12 +30,27 @@ def write_event(event):
     sys.stdout.buffer.flush()
 
 
-def push_key(input_event):
-    log_event(input_event, 'push_key')
-    input_event.value = VALUE.KEY_DOWN
-    write_event(input_event)
-    input_event.value = VALUE.KEY_UP
-    write_event(input_event)
+def make_key_event(code, value):
+    event = InputEvent()
+    event.code = code
+    event.value = value
+    event.type = TYPE.EV_KEY
+    return event
+
+
+def push_key(code, modifiers=None):
+
+    if modifiers is None:
+        modifiers = []
+
+    for mod_code in modifiers:
+        write_event(make_key_event(mod_code, VALUE.KEY_DOWN))
+
+    write_event(make_key_event(code, VALUE.KEY_DOWN))
+    write_event(make_key_event(code, VALUE.KEY_UP))
+
+    for mod_code in modifiers:
+        write_event(make_key_event(mod_code, VALUE.KEY_UP))
 
 
 class Modifier:
@@ -56,16 +73,17 @@ class Modifier:
         if input_event.value == VALUE.KEY_UP:
             self.is_space_down = False
             if self.is_actual:
-                push_key(input_event)
+                push_key(input_event.code)
 
     def handle_other_input(self, input_event):
-        self.is_actual = False
 
-        if self.is_space_down:
-            new_code = self.mapping.get(input_event.code, input_event.code)
-            input_event.code = new_code
-
-        write_event(input_event)
+        if not self.is_space_down:
+            write_event(input_event)
+        elif input_event.value == VALUE.KEY_DOWN:
+            self.is_actual = False
+            default_callback = lambda: push_key(input_event.code)
+            callback_function = self.mapping.get(input_event.code, default_callback)
+            callback_function()
 
 
     def handle_input(self, input_event):
@@ -75,15 +93,18 @@ class Modifier:
             self.handle_other_input(input_event)
 
 
-
-
 class SpaceModifier(Modifier):
     code = CODE.KEY_SPACE
     mapping = {
-        CODE.KEY_E: CODE.KEY_EQUAL,
-        CODE.KEY_W: CODE.KEY_MINUS,
-        CODE.KEY_F: CODE.KEY_APOSTROPHE,
+        CODE.KEY_W: lambda: push_key(CODE.KEY_MINUS),
+        CODE.KEY_E: lambda: push_key(CODE.KEY_EQUAL),
+        CODE.KEY_R: lambda: push_key(CODE.KEY_EQUAL, modifiers=[CODE.KEY_LEFTSHIFT]),
+        CODE.KEY_O: lambda: push_key(CODE.KEY_GRAVE, modifiers=[CODE.KEY_LEFTSHIFT]),
+        CODE.KEY_P: lambda: push_key(CODE.KEY_BACKSLASH, modifiers=[CODE.KEY_LEFTSHIFT]),
 
+        CODE.KEY_D: lambda: push_key(CODE.KEY_APOSTROPHE, modifiers=[CODE.KEY_LEFTSHIFT]),
+        CODE.KEY_F: lambda: push_key(CODE.KEY_APOSTROPHE),
+        CODE.KEY_U: lambda: push_key(CODE.KEY_MINUS, modifiers=[CODE.KEY_LEFTSHIFT]),
     }
 
 
@@ -100,18 +121,14 @@ def main():
             write_event(input_event)
             continue
 
-
         if input_event.code == CODE.KEY_1:
             exit(1)
         if input_event.code == CODE.KEY_2:
             log('\n\n\n\n\n\n\n')
             continue
 
-        # write_event(input_event)
-
         space_mod.handle_input(input_event)
         log(f'is space down: {space_mod.is_space_down}')
         log(f'is actual: {space_mod.is_actual}')
 
         log('*********************')
-
