4ecd345d833ee94c545f625488f3a1be29403147
Sam Luen-English <sam.luenenglish@atheon.co.uk>
2020-02-29 16:43:48 +0000

    Release keys before space press

--- pykeymapper/mappers/input_modifier.py
+++ pykeymapper/mappers/input_modifier.py
@@ -1,5 +1,5 @@
 from pykeymapper.events import VALUE
-from pykeymapper.utils import push_key, write_event
+from pykeymapper.utils import push_key, write_event, make_key_event
 
 
 class Modifier:
@@ -10,6 +10,11 @@ class Modifier:
     def __init__(self):
         self.is_mod_down = False
         self.is_actual = True
+        self.unreleased_keys = set([])
+
+    def release_keys(self):
+        for code in self.unreleased_keys:
+            write_event(make_key_event(code, VALUE.KEY_UP))
 
     def is_modifier_key(self, input_event):
         return input_event.code == self.code
@@ -19,6 +24,7 @@ class Modifier:
 
     def handle_mod_key_input(self, input_event):
         if input_event.value == VALUE.KEY_DOWN:
+            self.release_keys()
             self.is_mod_down = True
             self.is_actual = True
 
@@ -28,10 +34,18 @@ class Modifier:
                 push_key(self.get_release_code())
 
     def handle_other_input(self, input_event):
-
         if not self.is_mod_down:
             write_event(input_event)
-        elif input_event.value == VALUE.KEY_DOWN:
+
+            if input_event.value == VALUE.KEY_DOWN:
+                self.unreleased_keys.add(input_event.code)
+            if input_event.value == VALUE.KEY_UP:
+                try:
+                    self.unreleased_keys.remove(input_event.code)
+                except KeyError:
+                    pass
+
+        elif input_event.value in [VALUE.KEY_DOWN, VALUE.KEY_REPEAT]:
             self.is_actual = False
             try:
                 callback_function = self.mapping[input_event.code]
