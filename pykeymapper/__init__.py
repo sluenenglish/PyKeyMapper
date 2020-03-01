05e72de04c25cc5818535cca294f4ef4846bcc9e
Sam Luen-English <sam.luenenglish@atheon.co.uk>
2020-03-01 15:47:13 +0000

    v0.1.2

--- pykeymapper/__init__.py
+++ pykeymapper/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Samuel Luen-English"""
 __email__ = "sluenenglish@gmail.com"
-__version__ = "__version__ = '0.1.1'"
+__version__ = "__version__ = '0.1.2'"
 
 from logging import NullHandler
 
