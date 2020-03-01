f514b97cec6abd969332e09e0f3bbe09b4f0c3ce
Sam Luen-English <sam.luenenglish@atheon.co.uk>
2020-03-01 13:50:17 +0000

    Doc strings

--- pykeymapper/utils.py
+++ pykeymapper/utils.py
@@ -8,12 +8,31 @@ logger = logging.getLogger(__name__)
 
 
 def write_event(event):
+    """
+    Write event to standard out.
+
+    Args:
+        event(InputEvent): event to write.
+
+    """
     logger.debug(f"Writing event: {str(event)}")
     sys.stdout.buffer.write(event)
     sys.stdout.buffer.flush()
 
 
 def make_key_event(code, value, type=None):
+    """
+    Construct a input event.
+
+    Args:
+        code(int):
+        value(int):
+        type(Optional[type]):
+
+    Returns:
+        InputEvent:
+
+    """
     event = InputEvent()
     event.code = code
     event.value = value
@@ -22,6 +41,14 @@ def make_key_event(code, value, type=None):
 
 
 def push_key(code, modifiers=None):
+    """
+    Push a given key, with modifiers applied.
+
+    Args:
+        code(int): Key code to press.
+        modifiers(list of int): List of modifiers to press.
+
+    """
 
     if modifiers is None:
         modifiers = []
